c8f3f9634401ce83523b1c40f718f4d7237f10d1
Wladimir Guevara <wladige@gmail.com>
2020-09-05 14:09:36 -0400

    agregando bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-<div>
-  <input v-model="nombre" type="text" @keyup.enter="buscarPokemon" />
-  <button @click="buscarPokemon">Pokémon</button>
-  <div>
-    <img :src="imagen" alt />
-    <h6>{{nombrePokemon}}</h6>
-    <p v-for="(movimiento, i) in movimientos" :key="i">{{movimiento.move.name}}</p>
+  <div class="container">
+    <img class="img-fluid" width="900" src="../src/assets/pokemon_logo.png" alt />
+
+    <h3 class="text-center my-5">Ingresa un numero y presiona 'enter' para buscar un Pokemon</h3>
+    <input v-model="nombre" type="text" @keyup.enter="buscarPokemon" />
+    <button class="btn btn-primary m-1 p-1" @click="buscarPokemon">Pokémon</button>
+    <div class="row d-flex">
+      <div class="principal">
+        <img :src="imagen" alt />
+        <h6>{{nombrePokemon}}</h6>
+        <p v-for="(movimiento, i) in movimientos" :key="i">{{movimiento.move.name}}</p>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -48,12 +53,12 @@ export default {
       return this.pokemon.name;
     },
     movimientos() {
-      return this.pokemon.moves;
+      return this.pokemon.moves.slice(0, 5);
     },
     url() {
-      return this.nombre == "" ?
-        `${this.setUrl}pikachu` :
-        `${this.setUrl}${this.nombre.toLowerCase()}`;
+      return this.nombre == ""
+        ? `${this.setUrl}pikachu`
+        : `${this.setUrl}${this.nombre.toLowerCase()}`;
     },
     setUrl() {
       return "https://pokeapi.co/api/v2/pokemon/";
@@ -61,3 +66,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.principal {
+  background-image: url(../src/assets/poke.jpg);
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+</style>
